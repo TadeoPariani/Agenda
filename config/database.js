281300b73053ae37be3fa1ad9381dc34935cb9eb
TadeoPariani <tadeopariani1@gmail.com>
2023-05-25 22:44:10 -0300

    dd

--- config/database.js
+++ config/database.js
@@ -13,12 +13,4 @@ const testDbConnection = async () => {
   }
 };
 
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  } };
-
-
 module.exports = { sq: sequelize, testDbConnection };
